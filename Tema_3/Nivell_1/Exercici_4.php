9ae3ac30f7642a36f22e2712ec2e24633dd31445
Abel Galindo <abel.gp.1991@hotmail.es>
2023-05-03 09:51:31 +0200

    Tema 3 - Nivell 1 Final

---
+++ Tema_3/Nivell_1/Exercici_4.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$personal = [
+
+    "Nom" => "Abel",
+    "Edat" => 31,
+    "email" => "abel.gp.1991@hotmail.es",
+    "menjarFav" => "Sense preferències"
+
+];
+
+echo $personal ["Nom"] . "<br>";
+echo $personal ["Edat"] . "<br>";
+echo $personal ["email"] . "<br>";
+echo $personal ["menjarFav"] . "<br>";
+
+
+?>
