140cf5ef4d76da4224075f396d6074bd4a9f4b89
Abel Galindo <abel.gp.1991@hotmail.es>
2023-05-09 11:21:26 +0200

    Correcció Exercicis Tema 3

--- Tema_3/Nivell_1/Exercici_3.php
+++ Tema_3/Nivell_1/Exercici_3.php
@@ -9,42 +9,39 @@ $paraules = [
 
 ];
 
-$lletra = "p";
+$lletra = "h";
+
+// $control = true;
 
 // var_dump ($paraules);
 
 buscaLletra($paraules,$lletra);
 
 function buscaLletra($paraules,$lletra){
-
+    
     $control = true;
+    
+    for ($i=0;$i<sizeof($paraules) && $control;$i++){
+              
+        $control = str_contains($paraules[$i],$lletra);
+                   
+    }
+    
+    echo var_dump($control) . "<br>";
 
-    for ($i=0;$i<sizeof($paraules);$i++){
+    if ($control){
 
-        $coincidencia = strpos($paraules[$i],$lletra);
-
-        if ($coincidencia === false){
-
-        $control = false;
-        
-        break;
-
-        }
-
+        echo "La lletra " . $lletra . " es troba en totes les paraules!";
+    
+    }
+    
+    else{
+    
+        echo "La lletra " . $lletra . " no s'ha trobat en totes les paraules";
+    
     }
 
-if ($control === true){
-
-    echo "La lletra " . $lletra . " es troba en totes les paraules!";
-
 }
 
-else {
-
-    echo "La lletra " . $lletra . " no s'ha trobat en totes les paraules";
-
-}
-
-}
 
 ?>
--- Tema_3/Nivell_2/Exercici_1.php
+++ Tema_3/Nivell_2/Exercici_1.php
@@ -4,6 +4,19 @@
 $arrayNums1 = [1,2,3,4,5,6,11,7];
 $arrayNums2 = [1,3,5,7,9,11];
 
+$arrayInter = array_intersect($arrayNums1, $arrayNums2);
+
+echo "Els nombres en comú dels arrays són: <br> <br>";
+
+foreach ($arrayInter as $valor) {
+    
+    echo $valor . "<br>";
+}
+
+echo "<br>";
+
+/*
+
 for ($i=0; $i < sizeof($arrayNums1); $i++) { 
     
     if (in_array($arrayNums1[$i], $arrayNums2)==true){
@@ -13,5 +26,19 @@ for ($i=0; $i < sizeof($arrayNums1); $i++) {
     }
   
 }
+
+*/
+
+$arrayMezcla = array_merge($arrayNums1,$arrayNums2);
+
+echo "El resultat de la mescla dels arrays és: <br> <br>";
+
+foreach ($arrayMezcla as $valor) {
+    
+    echo $valor . "<br>";
+}
+
+echo "<br>";
+
 
 ?>
