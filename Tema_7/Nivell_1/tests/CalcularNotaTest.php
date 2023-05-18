35610bae349a687758e9ea248ac9b594a88a38a0
Abel Galindo <abel.gp.1991@hotmail.es>
2023-05-18 09:49:21 +0200

    Tema 7 - Nivell 1

---
+++ Tema_7/Nivell_1/tests/CalcularNotaTest.php
@@ -0,0 +1,48 @@
+
+<?php
+
+include "src/CalcularNota.php";
+
+use PHPUnit\Framework\TestCase;
+
+final class CalcularNotaTest extends TestCase{
+
+
+    public function ClassConstructorProvider(){
+
+        return [
+
+            [60, "L'alumne és de Primera Divisió"],
+            [45, "L'alumne és de Segona Divisió"],
+            [33, "L'alumne és de Tercera Divisió"],
+            [20, "L'alumne ha de recuperar"]
+
+        ];   
+
+    }
+
+
+/**
+ * @dataProvider ClassConstructorProvider
+ */
+    
+    public function testClassConstructor($valor,$divisio){
+   
+        $nota = new CalcularNota($valor);             
+            
+            $this->assertEqualsIgnoringCase($divisio,$nota->notes());          
+       
+        
+    }
+
+    
+    
+
+          
+
+
+}
+
+
+
+    
